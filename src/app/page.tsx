9a19505d50eb7def441e7048a12e3a6a0e3229c2
Dev Jeffrey <mdog1525@gmail.com>
2025-02-05 15:34:02 -0500

    set folders, components and files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+import About from "@/components/landing/About";
+import Contact from "@/components/landing/Contact";
+import Features from "@/components/landing/Features";
+import Main from "@/components/landing/Main";
+import Pricing from "@/components/landing/Pricing";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Bienvenido</h1>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p>@devjeffrey25</p>
-      </footer>
-    </div>
+    <>
+      <header>
+        <link rel="icon" href="/app/favicon.ico" sizes="any" />
+      </header>
+      <Main />
+      <About />
+      <Features />
+      <Pricing />
+      <Contact />
+    </>
   );
 }
