1976ce8271af0221a49669796393c5a683321122
Dev Jeffrey <mdog1525@gmail.com>
2025-02-14 09:41:03 -0500

    create script rewrites

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: true, // Opcional, para forzar URLs con "/"
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
